e314a1f2fd86da8eb21eb00741eeca97c3df6b53
Riccardo Righi <rr9379@gmail.com>
2018-12-19 10:24:03 +0100

    Swagger/OpenAPI v3 integration (wip)

--- swagger-v2/src/main/java/com/holonplatform/jaxrs/swagger/v2/internal/integration/SwaggerV2PropertyBoxModelConverter.java
+++ swagger-v2/src/main/java/com/holonplatform/jaxrs/swagger/v2/internal/integration/SwaggerV2PropertyBoxModelConverter.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.holonplatform.jaxrs.swagger.v2.internal.extensions;
+package com.holonplatform.jaxrs.swagger.v2.internal.integration;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
