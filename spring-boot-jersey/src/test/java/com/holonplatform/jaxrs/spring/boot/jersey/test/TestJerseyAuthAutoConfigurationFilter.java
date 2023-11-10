cdf39924c2b5a8d48b311493892bfc922153ae12
Fabio Paroni <fparoni@gmail.com>
2023-11-10 23:03:30 +0100

    Close #79, Close #80, Close #81, Close #82, Close #83, Close #84, Close #85,

--- spring-boot-jersey/src/test/java/com/holonplatform/jaxrs/spring/boot/jersey/test/TestJerseyAuthAutoConfigurationFilter.java
+++ spring-boot-jersey/src/test/java/com/holonplatform/jaxrs/spring/boot/jersey/test/TestJerseyAuthAutoConfigurationFilter.java
@@ -29,7 +29,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
